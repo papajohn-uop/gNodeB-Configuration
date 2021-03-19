fe1b2f0a0d68ace135dbcb1b04b780b59cd3d27b
Panagiotis Papaioannou <papajohn@upatras.gr>
2021-03-19 12:33:03 +0200

    Added getConnectionStatus endpoint

--- app/routes/system.py
+++ app/routes/system.py
@@ -1,5 +1,6 @@
 
 from fastapi import APIRouter
+import socket
 
 router = APIRouter()
 
@@ -8,7 +9,27 @@ router = APIRouter(
     tags=["system"],
 )
 
+system_route_resp=dict()
+system_route_resp["Module"]="Websocket related commands"
+system_route_resp["GET"]=dict()
+system_route_resp["GET"]["getConnectionStatus"]="Check connection status with target device"
+
+REMOTE_SERVER = "172.16.10.203"
+
+def _getConnectionStatus():
+    try:
+        host = socket.gethostbyname(REMOTE_SERVER)
+        socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+
 @router.get("/",  description="***** SYSTEM COMMANDS ROOT*****")
 async def system_root():
-    return [{"module": "System related commands"}]
+    return system_route_resp
 
+@router.get("/getConnectionStatus", description="***** Checks and returns connection status with target device*****", response_model=bool)
+async def ws_getConnectionStatus():
+        return _getConnectionStatus()
