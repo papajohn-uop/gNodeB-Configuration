ad6954e89d38e398b80a10425c188155154bf9b8
Panagiotis Papaioannou <papajohn@upatras.gr>
2021-03-20 12:06:00 +0200

    Created ctx object to better manage target gNodeB

--- app/main.py
+++ app/main.py
@@ -1,14 +1,84 @@
 from fastapi import Depends, FastAPI
 from routes import  system 
 
+import ipaddress
+
 
 app = FastAPI()
 
 
 app.include_router(system.router)
 
+
+
+class Target:
+    def __init__(self,user=None, password=None, IP=None):
+        self.description="Some desc"
+        if user is None:
+            self.user=None
+        else:
+            self.user= user
+
+        if password is None:
+            self.password=None
+        else:
+            self.password= password
+    
+    def setUser(self,user):
+        self.user=user
+
+    def setPass(self,password):
+        self.password=password
+
+    def setIP(self,IP):
+        self.IP= ipaddress.ip_address(IP)
+
+
+class Context:
+    def __init__(self,target=None):
+        self.self_info="Server info"
+        if target is None:
+            self.target=None
+        else:
+            self.target= target
+  
+    def setTarget(self,target):
+        self.target= target
+
+
+
+ctx=None
+
+
+def populateContext():
+    global ctx
+    if ctx is None:
+        target=Target()
+        target.setIP("172.16.10.203")
+        target.setUser("root")
+        target.setPass("toor")
+        ctx=Context(target)
+
+    
+    print(ctx.target.IP)
+
+@app.on_event("startup")
+async def startup_event():
+    print("Startup")
+    populateContext()
+    
+
+    ctx=Context()
+    if ctx.target:
+        if ctx.target.credentials:
+            if ctx.target.credentials.user:
+                print(ctx.target.credentials.user)
+   
+
 main_resp=dict()
 main_resp["/system"]="System related commands"
+
+
 
 @app.get("/")
 async def root():
