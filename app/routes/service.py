123a48304510aafb91a26a8c63464ec37e4483e9
Panagiotis Papaioannou <papajohn@upatras.gr>
2021-03-24 17:22:31 +0200

    Added start,stop,restart services

--- app/routes/service.py
+++ app/routes/service.py
@@ -18,6 +18,9 @@ service_route_resp=dict()
 service_route_resp["Module"]="Service related commands"
 service_route_resp["GET"]=dict()
 service_route_resp["GET"]["checkServiceStatus"]="checkServiceStatus"
+service_route_resp["GET"]["start"]="Start 5G Service"
+service_route_resp["GET"]["stop"]="Stop 5G service"
+service_route_resp["GET"]["restart"]="Restart 5G service"
 
 
 
@@ -45,3 +48,74 @@ async def system_ServiceStatus():
         return {"connectionStatus": res}
 
     
+@router.get("/start", description="***** Start the 5G service at  the target device*****")
+async def system_startService():
+    res=common._getConnectionStatus()
+    if res:
+        res=common._checkCredentials()
+        if res:
+            checkServiceStatusCMD=common._createCommand("service lte start")
+            print(checkServiceStatusCMD)
+            process = subprocess.Popen(checkServiceStatusCMD, stdout=subprocess.PIPE)
+            out=process.communicate()[0].decode('utf-8')
+            rc1=process.returncode
+            if rc1==0:
+                status="OK"
+            else:
+                status="NOT OK"
+            print(out)
+            print(rc1)
+            return {"Start Service": status}
+        else:
+            return {"credentialsVerified": res}
+    else:
+        return {"connectionStatus": res}
+
+
+@router.get("/stop", description="***** Stop the 5G service at  the target device*****")
+async def system_stopService():
+    res=common._getConnectionStatus()
+    if res:
+        res=common._checkCredentials()
+        if res:
+            checkServiceStatusCMD=common._createCommand("service lte stop")
+            print(checkServiceStatusCMD)
+            process = subprocess.Popen(checkServiceStatusCMD, stdout=subprocess.PIPE)
+            out=process.communicate()[0].decode('utf-8')
+            rc1=process.returncode
+            if rc1==0:
+                status="OK"
+            else:
+                status="NOT OK"
+            print(out)
+            print(rc1)
+            return {"Stop Service": status}
+        else:
+            return {"credentialsVerified": res}
+    else:
+        return {"connectionStatus": res}
+
+
+
+@router.get("/restart", description="***** Restart the 5G service at  the target device*****")
+async def system_restartService():
+    res=common._getConnectionStatus()
+    if res:
+        res=common._checkCredentials()
+        if res:
+            checkServiceStatusCMD=common._createCommand("service lte restart")
+            print(checkServiceStatusCMD)
+            process = subprocess.Popen(checkServiceStatusCMD, stdout=subprocess.PIPE)
+            out=process.communicate()[0].decode('utf-8')
+            rc1=process.returncode
+            if rc1==0:
+                status="OK"
+            else:
+                status="NOT OK"
+            print(out)
+            print(rc1)
+            return {"restart Service": status}
+        else:
+            return {"credentialsVerified": res}
+    else:
+        return {"connectionStatus": res}
