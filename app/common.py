2d076e31555578211d7e3b7ffaa132da73362d37
Panagiotis Papaioannou <papajohn@upatras.gr>
2021-03-24 17:08:56 +0200

    Added service route

---
+++ app/common.py
@@ -0,0 +1,40 @@
+from fastapi import Depends, FastAPI
+from routes import  system, service 
+
+import ipaddress
+import subprocess
+import socket
+
+import main
+
+def _getConnectionStatus():
+    try:
+        host = socket.gethostbyname(str(main.ctx.target.IP))
+        socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+
+
+CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh", "USER@IP","CMD"]
+
+def _createCommand(cmd):
+    myCtx=main.ctx
+    tmpCMD=CMD_TEMPLATE
+    tmpCMD[2]=myCtx.target.password
+    tmpCMD[4]='@'.join([myCtx.target.user,str(myCtx.target.IP)])
+    tmpCMD[5]=cmd
+    return tmpCMD
+
+def _checkCredentials():
+    checkCredCMD=_createCommand("exit")
+    process = subprocess.Popen(checkCredCMD, stdout=subprocess.PIPE)
+    out=process.communicate()[0].decode('utf-8')
+    rc1=process.returncode
+    print(out)
+    print(rc1)
+    return not(rc1)
+
+
