ca69cf544da0f7f19401febe909e2d680a270c93
Panagiotis Papaioannou <papajohn@upatras.gr>
2021-03-25 21:28:05 +0200

    Changes regarding ssh with  out stricthostchecking

--- app/common.py
+++ app/common.py
@@ -1,11 +1,11 @@
 from fastapi import Depends, FastAPI
-from routes import  system, service 
+
 
 import ipaddress
 import subprocess
 import socket
 
-import main
+from . import main
 
 def _getConnectionStatus():
     try:
@@ -18,16 +18,18 @@ def _getConnectionStatus():
 
 
 
-CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh", "USER@IP","CMD"]
-SCREEN_CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh","USER@IP","/root/REST_API/api_screen_log.sh", "CMD"]
+#CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh", "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no", "USER@IP","CMD"]
+CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh", "-o StrictHostKeyChecking=no ", "USER@IP","CMD"]
+#SCREEN_CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh","-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ", "USER@IP","/root/REST_API/api_screen_log.sh", "CMD"]
+SCREEN_CMD_TEMPLATE=["sshpass", "-p", "PASSWORD", "ssh","-o StrictHostKeyChecking=no ", "USER@IP","/root/REST_API/api_screen_log.sh", "CMD"]
 
 
 def _createCommand(cmd):
     myCtx=main.ctx
     tmpCMD=CMD_TEMPLATE
     tmpCMD[2]=myCtx.target.password
-    tmpCMD[4]='@'.join([myCtx.target.user,str(myCtx.target.IP)])
-    tmpCMD[5]=cmd
+    tmpCMD[5]='@'.join([myCtx.target.user,str(myCtx.target.IP)])
+    tmpCMD[6]=cmd
     return tmpCMD
 
 
@@ -35,18 +37,26 @@ def _createScreenCommand(cmd):
     myCtx=main.ctx
     tmpCMD=SCREEN_CMD_TEMPLATE
     tmpCMD[2]=myCtx.target.password
-    tmpCMD[4]='@'.join([myCtx.target.user,str(myCtx.target.IP)])
-    tmpCMD[6]=cmd
+    tmpCMD[5]='@'.join([myCtx.target.user,str(myCtx.target.IP)])
+    tmpCMD[7]=cmd
     return tmpCMD
 
 
 def _checkCredentials():
     checkCredCMD=_createCommand("exit")
+    print(checkCredCMD)
     process = subprocess.Popen(checkCredCMD, stdout=subprocess.PIPE)
     out=process.communicate()[0].decode('utf-8')
     rc1=process.returncode
     print(out)
     print(rc1)
+    if rc1==255 :
+        print("sshpass thnks we have MITM attack")
+        print("We should add target to ssh config")
+        print("QnD for now")
+        rc1=0
+        
+
     return not(rc1)
 
 
